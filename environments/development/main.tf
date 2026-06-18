673dd44f8ad0a27fb5ae29a97bbd80b314abf457
Ansu <ansuj9@yahoo.com>
2026-06-18 19:23:55 +0100

    fix: setting up acr

---
+++ environments/development/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false # Secure: Use OIDC/AAD auth instead of admin keys
+}
+
+# Assigns the AcrPush role to your OIDC Service Principal
+resource "azurerm_role_assignment" "acr_push" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPush"
+  principal_id         = var.service_principal_object_id
+}
+
