ed86688c1dcc82fbefa3462a82038e433651a3f0
Ansu <ansuj9@yahoo.com>
2026-06-20 20:06:42 +0100

    fix: add a optional azuread

--- environments/development/main.tf
+++ environments/development/main.tf
@@ -4,12 +4,18 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = "~> 2.0"
+    }
   }
 }
 
 provider "azurerm" {
   features {}
 }
+
+provider "azuread" {}
 
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
@@ -24,10 +30,22 @@ resource "azurerm_container_registry" "acr" {
   admin_enabled       = false # Secure: Use OIDC/AAD auth instead of admin keys
 }
 
+# Resolve a Service Principal by client (application) ID if provided
+data "azuread_service_principal" "sp" {
+  count         = length(trim(var.service_principal_client_id)) > 0 ? 1 : 0
+  application_id = var.service_principal_client_id
+}
+
+locals {
+  principal_id = length(trim(var.service_principal_object_id)) > 0 ? var.service_principal_object_id : (
+    length(data.azuread_service_principal.sp) > 0 ? data.azuread_service_principal.sp[0].object_id : ""
+  )
+}
+
 # Assigns the AcrPush role to your OIDC Service Principal
 resource "azurerm_role_assignment" "acr_push" {
   scope                = azurerm_container_registry.acr.id
   role_definition_name = "AcrPush"
-  principal_id         = var.service_principal_object_id
+  principal_id         = local.principal_id
 }
 
--- environments/development/variables.tf
+++ environments/development/variables.tf
@@ -17,4 +17,27 @@ variable "acr_name" {
 variable "service_principal_object_id" {
   type        = string
   description = "The Object ID (not Client ID) of the Service Principal used for OIDC"
+  default     = ""
+}
+
+variable "service_principal_client_id" {
+  type        = string
+  description = "The Client/Application ID of the Service Principal (optional). If provided, Terraform will resolve the Object ID via the azuread provider."
+  default     = ""
+}
+
+variable "service_principal_validation" {
+  type    = bool
+  default = true
+  description = "(internal) validation toggle - do not set"
+}
+
+/* Ensure at least one of object_id or client_id is provided */
+variable "service_principal_input_check" {
+  type = string
+  default = ""
+  validation {
+    condition = (length(trim(var.service_principal_object_id)) > 0) || (length(trim(var.service_principal_client_id)) > 0)
+    error_message = "Provide either service_principal_object_id (Object ID) or service_principal_client_id (Application/Client ID)."
+  }
 }
